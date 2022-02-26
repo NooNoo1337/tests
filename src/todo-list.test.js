371e7347c6efffa3fcc06ddf2579ab192c2ab5f1
Mikhail Cheremuhin-Rerberg <mcheremukhin-rerberg@alfabank.ru>
2022-02-26 19:53:04 +0300

    add stubs

--- src/todo-list.test.js
+++ src/todo-list.test.js
@@ -1,6 +1,9 @@
-
 const { it, expect } = require("@jest/globals");
-const { TodoList, TodoItem } = require("./todo-list");
+const { randomUUID } = require("crypto");
+const { TodoList } = require("./todo-list");
+
+// Объявляем, что модуль мокается
+jest.mock("crypto");
 
 it("should add first element to list", () => {
   const list = new TodoList();
@@ -11,12 +14,41 @@ it("should add first element to list", () => {
   expect(list.items[0].name).toBe("create test");
 });
 
-it('should add second item to list', () => {
-  const item = new TodoItem({ name: "create test" });
-  const list = new TodoList([item]);
+it("should add second item to list", () => {
+  const list = new TodoList(["create test"]);
 
   list.addItem({ name: "check me" });
 
   expect(list.items).toHaveLength(2);
   expect(list.items[1].name).toBe("check me");
+});
+
+it("should load items", async () => {
+  // Ставим мок (заглушку)
+  randomUUID.mockImplementation(() => "00000000-0000-0000-0000-000000000000");
+  const list = new TodoList();
+
+  list.fetch = () => {
+    return Promise.resolve(["item1", "item2", "item3"]);
+  };
+
+  await list.load();
+
+  expect(list.items).toEqual([
+    {
+      name: "item1",
+      isDone: false,
+      id: "00000000-0000-0000-0000-000000000000",
+    },
+    {
+      name: "item2",
+      isDone: false,
+      id: "00000000-0000-0000-0000-000000000000",
+    },
+    {
+      name: "item3",
+      isDone: false,
+      id: "00000000-0000-0000-0000-000000000000",
+    },
+  ]);
 });
