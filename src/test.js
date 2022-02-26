63dd3f6d31afea40e9ae95f86642e97846b0b05f
Mikhail Cheremuhin-Rerberg <mcheremukhin-rerberg@alfabank.ru>
2022-02-26 18:09:48 +0300

    add second test case

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-const { TodoList } = require("./todo-list");
+const { TodoList, TodoItem } = require("./todo-list");
 
 const testAddFirstItem = () => {
   // Подготовка
@@ -11,6 +11,18 @@ const testAddFirstItem = () => {
   if(list.items.length === 0) {
     throw Error('Items equal to 0')
   }
+}
+
+const testSecondAddFirstItem = () => {
+  const item = new TodoItem({ name: "create test" });
+  const list = new TodoList([item]);
+
+  list.addItem({ name: "check me" });
+
+  if (list.items[1].name !== "check me") {
+    throw Error("Second item was not added");
+  }
 };
 
 testAddFirstItem();
+testSecondAddFirstItem();
